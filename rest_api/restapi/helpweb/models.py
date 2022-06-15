7bdf7fa05211aa6a4d2736cd3cb13f491be4074e
강기범 <kindgivum@gmail.com>
2022-06-15 15:05:17 +0900

    웹 회원가입, 로그인 ok / 기본페이지 없음

---
+++ rest_api/restapi/helpweb/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Manager(models.Model):
+    mno = models.IntegerField(primary_key=True, auto_created=True)
+    idweb = models.CharField(max_length=10, blank=False)
+    passwordweb = models.CharField(max_length=20, blank=False)
+    emailweb = models.CharField(max_length=100, blank=False)
+    phoneweb = models.CharField(max_length=11, blank=False)
+    addressweb = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        db_table = 'manager'
